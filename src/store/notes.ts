263c67fab94c7a7d1040585dd262d5ce5ca56f1a
mizuki_r <ry.mizuki@gmail.com>
2019-07-26 07:50:43 +0900

    use vuex-module-decorators

--- src/store/notes.ts
+++ src/store/notes.ts
@@ -1,52 +1,35 @@
-import {
-  NoteEntity,
-  NotesState,
-  RootState,
-  NotesGetters,
-  NotesMutations,
-  NotesActions
-} from './types'
-import { DefineGetters, DefineMutations, DefineActions } from 'vuex-type-helper'
+import { NoteEntity, NoteForm } from './types'
+import { VuexModule, Mutation, Action } from 'vuex-module-decorators'
 
-export const namespaced = true
+export default class NotesModule extends VuexModule {
+  rows: NoteEntity[] = []
 
-export const state: NotesState = { rows: [] }
-
-export const getters: DefineGetters<NotesGetters, NotesState, RootState> = {
-  rows(state) {
-    return () => state.rows
+  @Mutation
+  FETCH_RESOLVE(rows: NoteEntity[]) {
+    this.rows = rows
   }
-}
 
-export const mutations: DefineMutations<NotesMutations, NotesState> = {
-  FETCH_RESOLVE(state, { rows }) {
-    state.rows = rows
-  },
-  ADD_RESOLVE(state, { form }) {
-    const lastId = state.rows[state.rows.length - 1].id
+  @Mutation
+  ADD_RESOLVE(form: NoteForm) {
+    const lastId = this.rows[this.rows.length - 1].id
     const note: NoteEntity = {
       id: lastId + 1,
       content: form.content,
       created: new Date()
     }
-    state.rows.push(note)
+    this.rows.push(note)
   }
-}
 
-export const actions: DefineActions<
-  NotesActions,
-  NotesState,
-  NotesMutations,
-  RootState
-> = {
-  async fetch(ctx) {
+  @Action
+  async fetch() {
     const rows: NoteEntity[] = [
       { id: 1, content: 'test', created: new Date('2019-07-24 23:47:00') }
     ]
-    ctx.commit('FETCH_RESOLVE', { rows })
-  },
+    return rows
+  }
 
-  async add(ctx, { form }) {
-    ctx.commit('ADD_RESOLVE', { form })
+  @Action
+  async add(form: NoteForm) {
+    return form
   }
 }
